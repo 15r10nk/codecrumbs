ab9fcb892b093e0ebb533af0d91cb511ce65aed7
Frank Hoffmann <15r10nk-git@polarbit.de>
2022-12-12 20:34:47 +0100

    test: correct coverage

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import nox
 
 nox.options.sessions = ["clean", "test", "report", "docs"]
@@ -6,7 +8,8 @@ nox.options.reuse_existing_virtualenvs = True
 
 @nox.session(python="python3.10")
 def clean(session):
-    session.install("coverage[toml]")
+    session.run_always("poetry", "install", "--with=dev", external=True)
+    session.env["TOP"] = str(Path(__file__).parent)
     session.run("coverage", "erase")
 
 
@@ -17,29 +20,25 @@ def mypy(session):
     session.run("mypy", "src", "tests")
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.9", "3.10", "3.11"])
 def test(session):
     session.run_always("poetry", "install", "--with=dev", external=True)
+    session.env["COVERAGE_PROCESS_START"] = str(
+        Path(__file__).parent / "pyproject.toml"
+    )
+    session.env["TOP"] = str(Path(__file__).parent)
     session.run(
-        "pytest",
-        "--cov=src",
-        "--cov=tests",
-        "--cov=codecrumbs",
-        "--cov-append",
-        "--cov-branch",
-        "--doctest-modules",
-        "src/codecrumbs",
-        "tests",
-        *session.posargs
+        "pytest", "--doctest-modules", "src/codecrumbs", "tests", *session.posargs
     )
 
 
 @nox.session(python="python3.10")
 def report(session):
-    session.install("coverage[toml]")
-    # session.run("coverage", "combine")
+    session.run_always("poetry", "install", "--with=dev", external=True)
+    session.env["TOP"] = str(Path(__file__).parent)
+    session.run("coverage", "combine")
     session.run("coverage", "html")
-    session.run("coverage", "report", "--fail-under", "92")
+    session.run("coverage", "report", "--fail-under", "94")
 
 
 @nox.session(python="python3.10")
